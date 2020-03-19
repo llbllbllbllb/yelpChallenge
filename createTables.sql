7e86f19b96c7d594c0c569f165c3c367b34c008d
llbllbllbllb <295319725@qq.com>
2020-03-19 11:26:08 -0400

    first draft of create tables

--- createTables.sql
+++ createTables.sql
@@ -17,7 +17,7 @@ CREATE TABLE business (
   is_open INT,
 
 
-  PRIMARY KEY(user_id)
+  PRIMARY KEY(business_id)
 );
 
 
@@ -37,6 +37,11 @@ CREATE TABLE attributes (
 DROP TABLE IF EXISTS categories;
 -- SHOW WARNINGS;
 CREATE TABLE categories (
+  business_id CHAR(22) NOT NULL,
+  category VARCHAR(30) NOT NULL,
+
+  PRIMARY KEY(business_id,category),
+  FOREIGN KEY(business_id) REFERENCES business(business_id)
 
 );
 
@@ -49,5 +54,113 @@ CREATE TABLE categories (
 DROP TABLE IF EXISTS hours;
 -- SHOW WARNINGS;
 CREATE TABLE hours (
+  business_id CHAR(22) NOT NULL,
+
+  mondayStart TIME,
+  mondayEnd TIME,
+
+  tuesdayStart TIME,
+  tuesdayEnd TIME,
+
+  wednesdayStart TIME,
+  wednesdayEnd TIME,
+
+  thursdayStart TIME,
+  thursdayEnd TIME,
+
+  fridayStart TIME,
+  fridayEnd TIME,
+
+  saturdayStart TIME,
+  saturdayEnd TIME,
+
+  sundayStart TIME,
+  sundayEnd TIME,
+
+  PRIMARY KEY(business_id),
+  FOREIGN KEY(business_id) REFERENCES business(business_id)
+);
+
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS user;
+-- SHOW WARNINGS;
+CREATE TABLE user (
+  user_id CHAR(22) NOT NULL,
+  name VARCHAR(100),
+  review_count INT DEFAULT 0,
+  yelping_since DATE,
+  useful INT DEFAULT 0,
+  funny INT DEFAULT 0,
+  cool INT DEFAULT 0,
+  fans INT DEFAULT 0,
+  average_stars FLOAT,
+  compliment_hot INT DEFAULT 0,
+  compliment_more INT DEFAULT 0,
+  compliment_profile INT DEFAULT 0,
+  compliment_cute INT DEFAULT 0,
+  compliment_list INT DEFAULT 0,
+  compliment_note INT DEFAULT 0,
+  compliment_plain INT DEFAULT 0,
+  compliment_cool INT DEFAULT 0,
+  compliment_funny INT DEFAULT 0,
+  compliment_writer INT DEFAULT 0,
+  compliment_photos INT DEFAULT 0,
+
+  PRIMARY KEY(user_id)
+
+);
+
+-- ----------------------------
+--  Table structure for `checkin`
+-- ----------------------------
+DROP TABLE IF EXISTS checkin;
+-- SHOW WARNINGS;
+CREATE TABLE checkin (
+  business_id CHAR(22) NOT NULL,
+  checkinDate DATE NOT NULL,
+
+  PRIMARY KEY(business_id,checkinDate),
+  FOREIGN KEY(business_id) REFERENCES business(business_id)
+
+);
+
+-- ----------------------------
+--  Table structure for `tip`
+-- ----------------------------
+DROP TABLE IF EXISTS tip;
+-- SHOW WARNINGS;
+CREATE TABLE tip (
+  business_id CHAR(22) NOT NULL,
+  user_id CHAR(22) NOT NULL,
+
+  tipText VARCHAR(200) NOT NULL,
+  postDate DATE NOT NULL,
+  compliment_count INT DEFAULT 0 NOT NULL,
+
+  PRIMARY KEY(business_id, user_id),
+  FOREIGN KEY(business_id) REFERENCES business(business_id),
+  FOREIGN KEY(user_id) REFERENCES user(user_id)
+
+);
+
+
+-- ----------------------------
+--  Table structure for `photo`
+-- ----------------------------
+DROP TABLE IF EXISTS photo;
+-- SHOW WARNINGS;
+CREATE TABLE photo (
+  photo_id CHAR(22) NOT NULL,
+  business_id CHAR(22) NOT NULL,
+
+  caption VARCHAR(100),
+  label VARCHAR(10),
+
+  PRIMARY KEY(photo_id),
+  FOREIGN KEY(business_id) REFERENCES business(business_id),
+
 
 );
