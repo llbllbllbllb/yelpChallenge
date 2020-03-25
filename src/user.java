5d34d9b1282afcb5647f748e0f0e612634cfc90b
llbllbllbllb <295319725@qq.com>
2020-03-25 15:11:05 -0400

    writeReview api

--- src/user.java
+++ src/user.java
@@ -2,6 +2,8 @@ import java.sql.*;
 import java.time.Year;
 import java.util.ArrayList;
 import java.time.LocalDate;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
 import java.util.Random;
 
 public class user extends dbConnection{
@@ -231,7 +233,7 @@ public class user extends dbConnection{
         }
     }
 
-    public void createGroup(String groupname, ArrayList<String> friends, Connection conn) {
+    public void createGroup(String groupname, ArrayList<String> friends, Connection conn) throws SQLException{
         if(groupname == null || groupname.length() == 0) {
             System.out.println("Error: Empty group name.");
             return;
@@ -278,7 +280,7 @@ public class user extends dbConnection{
 
     }
 
-    public void joinGroup(String group_id, Connection conn) {
+    public void joinGroup(String group_id, Connection conn) throws SQLException{
         String sql = "INSERT INTO user_group (group_id,user_id) VALUES (\'"+group_id+"\',\'"+this.user_id+"\');";
 
         try {
@@ -296,7 +298,7 @@ public class user extends dbConnection{
 
     }
 
-    public void followBusiness(String business_id, Connection conn) {
+    public void followBusiness(String business_id, Connection conn) throws SQLException{
         String sql = "INSERT INTO user_follow_business (user_id,business_id) VALUES (\'"+this.user_id+"\',\'"+business_id+"\');";
 
         try {
@@ -311,6 +313,41 @@ public class user extends dbConnection{
             conn.rollback();
             e.printStackTrace();
         }
+    }
+
+    public void writeReview(String business_id, int stars, String review_text, Connection conn) throws SQLException{
+        String review_id = generateUniqueId();
+
+        //get current date and time
+        LocalDateTime curTime = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String reviewDate = curTime.format(dateFormat);
+        String reviewTime = curTime.format(timeFormat);
+
+        // replace some text to avoid insert failure
+        review_text = review_text.replace('"',"");
+        review_text = review_text.replace('\\',"/");
+        reviewText = reviewText.replace(';'," ");
+
+        String sql = "INSERT INTO review (review_id, user_id, business_id, stars, reviewDate, reviewTime, reviewText) VALUES "
+                + "(\'" + review_id + "\',\'" + this.user_id + "\',\'" + business_id + "\',\'" + stars + "\',\'" + reviewDate + "\',\'" + reviewTime + "\',\"" + review_text + "\");";
+
+
+        try {
+            Boolean status = insertSQL(sql, conn);
+            if (!status) {
+                throw new SQLException("Can not write review, Please try again");
+            }
+            conn.commit();
+            System.out.println("Review written successfully");
+
+        } catch (SQLException e){
+            conn.rollback();
+            e.printStackTrace();
+        }
+
+
     }
 
 
